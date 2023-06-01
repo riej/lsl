8e67cc9e5880103714f27bf0e8bfe841150b08e5
riejinn <riejinn@gmail.com>
2023-06-01 18:29:47 +0500

    fix arguments update
    silly me

--- src/main/kotlin/io/github/riej/lsl/psi/LslArguments.kt
+++ src/main/kotlin/io/github/riej/lsl/psi/LslArguments.kt
@@ -11,17 +11,6 @@ import com.intellij.util.FileContentUtilCore
 import io.github.riej.lsl.parser.LslTypes
 
 class LslArguments(node: ASTNode) : ASTWrapperPsiElement(node) {
-    val arguments: List<LslArgument> = findChildrenByType(LslTypes.ARGUMENT)
-
-    override fun subtreeChanged() {
-        // There is problem when modification of function arguments doesn't affects to function body.
-        // For example:
-        // integer meow() { return a + b; }
-        // IDE will show errors at `a` and `b` - because they does not exists yet.
-        // But if you write arguments (`integer a, integer b`), IDE still will show errors.
-        // I really have no idea how to fix this behavior except reparsing whole file.
-        // This is slow, but it works.
-        // TODO: find proper solution.
-        FileContentUtilCore.reparseFiles(this.containingFile.virtualFile)
-    }
+    val arguments: List<LslArgument>
+        get() = findChildrenByType(LslTypes.ARGUMENT)
 }
