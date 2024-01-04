93c2d4c918e563147fd24d896b24e7bf96ecb0b9
riejinn <riejinn@gmail.com>
2024-01-04 19:02:29 +0500

    fix unreachable code inspection for labels (#20)
    thanks to @spflanze
    
    new code has some bug too - it marks as reachable some unreachable code. but at least it's better than it was before.

--- src/main/kotlin/io/github/riej/lsl/inspections/LslUnreachableCodeInspection.kt
+++ src/main/kotlin/io/github/riej/lsl/inspections/LslUnreachableCodeInspection.kt
@@ -5,7 +5,11 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.search.LocalSearchScope
+import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.parentOfType
+import com.intellij.psi.util.parents
 import io.github.riej.lsl.LslLanguage
 import io.github.riej.lsl.psi.*
 
@@ -19,32 +23,73 @@ class LslUnreachableCodeInspection : LocalInspectionTool() {
 
         PsiTreeUtil.collectElementsOfType(file, LslStatementBlock::class.java)
             .filter { !it.textRange.isEmpty }
-            .forEach {
-                val unreachableChildren = it.children.dropWhile {
-                    when (it) {
-                        is LslStatementReturn -> return@dropWhile false
-                        is LslStatementState -> return@dropWhile false
-                        is LslStatementExpression -> return@dropWhile !isFinalFunctionCall(it.children.singleOrNull() as? LslExpressionFunctionCall)
+            .forEach { block ->
+                val unreachableCodeRanges = findUnreachableCodeRanges(block)
 
-                        else -> return@dropWhile true
-                    }
-                }.drop(1)
-
-                if (unreachableChildren.isNotEmpty()) {
+                unreachableCodeRanges.forEach {
                     problemsHolder.registerProblem(
-                        it,
+                        block,
                         "Unreachable code",
                         ProblemHighlightType.LIKE_UNUSED_SYMBOL,
-                        TextRange(unreachableChildren.first().startOffsetInParent, it.textLength - 1),
+                        TextRange(it.first().startOffsetInParent, it.last().textRangeInParent.endOffset),
                         RemoveUnreachableCodeFix(
-                            unreachableChildren.first(),
-                            it.lastChild.prevSibling, // lastChild is "}", so take it's previous sibling
+                            it.first(),
+                            it.last()
                         )
                     )
                 }
             }
 
         return problemsHolder.resultsArray
+    }
+
+    private fun findUnreachableCodeRanges(block: LslStatementBlock): List<Array<PsiElement>> {
+        var isReachable = true
+        val currentUnreachableBlock = ArrayList<PsiElement>()
+        val result = ArrayList<Array<PsiElement>>()
+
+        val searchScope = LocalSearchScope(
+            block.parents(false).filter { it is LslFunction || it is LslEvent }.first()
+        )
+
+        block.children.forEach {
+            // if code is unreachable, but it's label with existing jump, make it reachable again
+            // but this code will mark as reachable this:
+            //   return;
+            //   @someLabel; // label has jump - so...
+            //   jump someLabel;
+            //   llOwnerSay("meow");
+            // but well, it already works better than before
+            // TODO: follow all code branches and find truly unreachable ones
+            if (!isReachable && (it is LslStatementLabel) && ReferencesSearch.search(it, searchScope).findFirst() != null) {
+                isReachable = true
+
+                if (currentUnreachableBlock.isNotEmpty()) {
+                    result.add(currentUnreachableBlock.toTypedArray())
+                    currentUnreachableBlock.clear()
+                }
+            }
+
+            if (!isReachable) {
+                currentUnreachableBlock.add(it)
+            }
+
+            // make code unreachable if it meets return, state switch or final function call (llDie, llResetScript)
+            isReachable = isReachable && when (it) {
+                is LslStatementReturn -> false
+                is LslStatementState -> false
+                is LslStatementExpression -> !isFinalFunctionCall(it.children.singleOrNull() as? LslExpressionFunctionCall)
+
+                else -> true
+            }
+        }
+
+        if (currentUnreachableBlock.isNotEmpty()) {
+            result.add(currentUnreachableBlock.toTypedArray())
+            currentUnreachableBlock.clear()
+        }
+
+        return result
     }
 
     private fun isFinalFunctionCall(functionCall: LslExpressionFunctionCall?): Boolean {
